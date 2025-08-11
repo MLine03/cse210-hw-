5172b213d8918c79baf668c8bb27a008d5b8572c
Meline Levao <your-github-email@example.com>
2025-08-11 14:53:48 -0600

    Fix namespace and project structure to resolve Cycling class not found error

--- week07/ExerciseTracking/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/ExerciseTracking/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExerciseTracking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Activity> activities = new List<Activity>
+            {
+                new Running(new DateTime(2022, 11, 3), 30, 4.8),
+                new Cycling(new DateTime(2022, 11, 3), 45, 20.1),
+                new Swimming(new DateTime(2022, 11, 3), 40, 30)
+            };
+
+            foreach (Activity activity in activities)
+            {
+                Console.WriteLine(activity.GetSummary());
+            }
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
