b53f0754439eab9ae55e659e02855fe6ae546c5d
Meline Levao <your-github-email@example.com>
2025-08-11 14:44:44 -0600

    Add Exercise Tracking Program with Activity, Running, Cycling, and Swimming classes

---
+++ week07/Cycling.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ExerciseTracking
+{
+    public class Cycling : Activity
+    {
+        private double _speedKph;
+
+        public Cycling(DateTime date, int minutes, double speedKph)
+            : base(date, minutes)
+        {
+            _speedKph = speedKph;
+        }
+
+        public override double GetDistance() => (_speedKph * Minutes) / 60;
+
+        public override double GetSpeed() => _speedKph;
+
+        public override double GetPace() => 60 / _speedKph;
+    }
+}
--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,26 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
 
-class Program
+namespace ExerciseTracking
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        static void Main(string[] args)
+        {
+            List<Activity> activities = new List<Activity>
+            {
+                new Running(new DateTime(2022, 11, 3), 30, 4.8),
+                new Cycling(new DateTime(2022, 11, 3), 45, 20.1),
+                new Swimming(new DateTime(2022, 11, 3), 40, 30)
+            };
+
+            foreach (Activity activity in activities)
+            {
+                Console.WriteLine(activity.GetSummary());
+            }
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
     }
 }
